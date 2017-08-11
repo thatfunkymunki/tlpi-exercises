55e6b4aa14d98b425455a1698e32ffd51c54c790
munki <thatfunkymunki@gmail.com>
2017-08-10 19:22:37 -1000

    working readv!

--- readvwritev.c
+++ readvwritev.c
@@ -9,11 +9,10 @@
 #include <fcntl.h>
 #include <sys/uio.h>
 /*  struct iovec {
- *    void *iov_ base; //start address of buffer
+ *    void *iov_base; //start address of buffer
  *    ssize_t iov_len; //number of bytes to transfer to/from buffer
  *  }
  */
-#define IOV_FOREACH(i, iovcnt) for(i=0;i<iovcnt;i++)
 ssize_t _readv(int fd, const struct iovec *iov, int iovcnt);
 //returns # of bytes read, 0 on eof, or -1 on error
 
@@ -22,25 +21,58 @@ ssize_t _writev(int fd, const struct iovec *iov, int iovcnt);
 
 
 ssize_t _readv(int fd, const struct iovec *iov, int iovcnt){
-  ssize_t numread=0;
-  ssize_t *buffer;
-  size_t bufsize=0;
-  int i;
- 
-  //loop over the iov array
-  IOV_FOREACH(i,iovcnt){
-    //allocate sum(iov[n].iov_len) bytes for buffer
+  //find out how many bytes to read
+  //allocate buffer to read that many bytes
+  //read that many bytes
+  //scatter that into iov **ensure that n < num of bytes read
+  //  for each iovec i in iov
+  //    for j=0..iov[i]->iov_len
+  //      iov[i].iov_base[j]=buffer[n]
+  //      n++
+  
+  int bufsize=0;
+  char *buffer;
+  ssize_t numread = 0;
+  for(int i=0;i<iovcnt;i++){
+    printf("adding %ld to bufsize\n",iov[i].iov_len);
     bufsize+=iov[i].iov_len;
   }
-  buffer = malloc(bufsize*sizeof(ssize_t));
-  //read iov[n].iov_len number of bytes
-  numread = read(fd,buffer,bufsize);
-  //populate the vectors
-  IOV_FOREACH(i,iovcnt){
+  buffer = malloc(bufsize);
 
+  numread = read(fd, buffer, bufsize);
+
+  for(int i=0, n=0;i<iovcnt && n<numread; i++){
+    for(int j = 0; j < iov[i].iov_len && n<numread; j++){
+      char *target = ((char*)iov[i].iov_base) + j;
+      *target = buffer[n];
+      n++;
+    }
   }
   return numread;
+  
 }
 int main(int argc, char** argv){
-  return 0;
+  struct iovec test[3];
+
+  test[0].iov_len = 5;
+  test[0].iov_base = malloc(5* sizeof(char));
+
+  test[1].iov_len = 15;
+  test[1].iov_base = malloc(15* sizeof(char));
+
+  test[2].iov_len = 6;
+  test[2].iov_base = malloc(6 * sizeof(char));
+
+  int fd = open(argv[1], O_RDONLY);
+
+  int numread = _readv(fd, test, 3);
+  printf("num read: %d\n", numread);
+
+  for(int i =0; i<3; i++){
+
+    for(int j=0;j<test[i].iov_len; j++){
+      printf("%d: %d: %x\n",i,j, *((char *)test[i].iov_base+j));
+
+    }
+  }
 }
