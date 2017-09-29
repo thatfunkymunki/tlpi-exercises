d5c5a17e048ddba69083508e80449fcd88b78b51
munki man <thatfunkymunki@gmail.com>
2017-09-28 19:28:12 -0500

    mostly working malloc() and free()

--- malloc.c
+++ malloc.c
@@ -1,46 +1,113 @@
-/* Exercise 5-7
- * Implement readv() and writev() using read(), write(), and suitable functions from the
- * malloc package (Section 7.1.2).
+/* Exercise 7-4
+ * (Advanced) Implement malloc() and free()
+ *
+ * Sources for reference: 
+ * https://danluu.com/malloc-tutorial/ 
+ * http://tharikasblogs.blogspot.com/p/how-to-write-your-own-malloc-and-free.html
  */
 #include "tlpi_hdr.h"
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/uio.h>
-/*  struct iovec {
- *    void *iov_ base; //start address of buffer
- *    ssize_t iov_len; //number of bytes to transfer to/from buffer
- *  }
- */
-#define IOV_FOREACH(i, iovcnt) for(i=0;i<iovcnt;i++)
-ssize_t _readv(int fd, const struct iovec *iov, int iovcnt);
-//returns # of bytes read, 0 on eof, or -1 on error
 
-ssize_t _writev(int fd, const struct iovec *iov, int iovcnt);
-//returns number of bytes written or -1 on error
+//prototypes
+void *_malloc(size_t size);
+void _free(void *ptr);
+enum status { FREE, USED };
+struct block *find_free_block(struct block **last, size_t size);
+struct block *allocate_space(struct block *last, size_t size);
+struct block *get_block(void *ptr);
 
 
-ssize_t _readv(int fd, const struct iovec *iov, int iovcnt){
-  ssize_t numread=0;
-  ssize_t *buffer;
-  size_t bufsize=0;
-  int i;
- 
-  //loop over the iov array
-  IOV_FOREACH(i,iovcnt){
-    //allocate sum(iov[n].iov_len) bytes for buffer
-    bufsize+=iov[i].iov_len;
+
+//top of heap linked list
+void *head = NULL;
+
+//need to create a metadata struct for each region
+struct block{
+  size_t size;
+  struct block *next;
+  enum status free;
+};
+
+struct block *find_free_block(struct block **last, size_t size){
+  struct block *current = head;
+  while(current !=NULL && !(current->free == FREE && current->size >= size)){
+    *last=current;
+    current=current->next;
   }
-  buffer = malloc(bufsize*sizeof(ssize_t));
-  //read iov[n].iov_len number of bytes
-  numread = read(fd,buffer,bufsize);
-  //populate the vectors
-  IOV_FOREACH(i,iovcnt){
-
-  }
-  return numread;
+  return current;
 }
+
+struct block *allocate_space(struct block *last, size_t size){
+  struct block *new;
+  new = sbrk(0);
+  void *request = sbrk(size+sizeof(struct block));
+  if(request == (void *) -1){
+    return NULL;
+  }
+  
+  if(last != NULL){
+    last->next = new;
+  }
+  new->size = size;
+  new->next = NULL;
+  new->free = USED;
+  return new;
+}
+
+void *_malloc(size_t size){
+  struct block *new;
+  
+  if(size<=0){
+    return NULL;
+  }
+  
+  if(head == NULL){
+    new = allocate_space(NULL,size);
+    if(new == NULL){
+      return NULL;
+    }
+    head = new;
+  }
+  else{
+    struct block *last = head;
+    new = find_free_block(&last, size);
+    if(new == NULL){
+      new = allocate_space(last,size);
+      if(new==NULL){
+        return NULL;
+      }
+    }
+    else{
+      new->free = USED;
+    }
+  }
+  return new+1; //leave space for a block struct
+}
+struct block *get_block(void *ptr){
+  return (struct block *)ptr-1;
+  
+}
+
+void _free(void *ptr){
+  if(ptr == NULL){
+    return;
+  }
+  struct block *ptr_block = get_block(ptr);
+  ptr_block->free = FREE;
+}
+
 int main(int argc, char** argv){
+
+  printf("program break: %p\n", sbrk(0));
+  
+  char **newstr = _malloc(sizeof(char *)*8);
+  for (int i = 0; i < 8; i++){
+    newstr[i]= _malloc(sizeof(char)*8);
+    sprintf(newstr[i],"%d%d%d%d%d%d%d",i,i,i,i,i,i,i);
+  }
+  for (int i = 0; i < 8; i++){
+    printf("%p: %s\n",(void *)&newstr[i], newstr[i]);
+    _free(newstr[i]);
+  }
+  
   return 0;
 }
