31aac78b7e0d7a6dcc6ac36943e97152b6e53dda
munki man <thatfunkymunki@gmail.com>
2017-10-12 15:14:29 -0500

    corrected exercise heading for 12-2

--- 12-2-pstree.c
+++ 12-2-pstree.c
@@ -1,13 +1,12 @@
-/*Exercise 12-1
- * Write a program that lists the process ID and command name for all processes
- * being run by the user named in the program’s command-line argument. (You may
- * find the userIdFromName() function from Listing 8-1, on page 159, useful.) This can
- * be done by inspecting the Name: and Uid: lines of all of the /proc/PID/status files on
- * the system. Walking through all of the /proc/PID directories on the system requires the
- * use of readdir(3), which is described in Section 18.8. Make sure your program
- * correctly handles the possibility that a /proc/PID directory disappears between the
- * time that the program determines that the directory exists and the time that it tries
- * to open the corresponding /proc/PID/status file.
+/*Exercise 12-2
+ * Write a program that draws a tree showing the hierarchical parent-child
+ * relationships of all processes on the system, going all the way back to init. For each
+ * process, the program should display the process ID and the command being
+ * executed. The output of the program should be similar to that produced by pstree(1),
+ * although it does need not to be as sophisticated. The parent of each process on the
+ * system can be found by inspecting the PPid: line of all of the /proc/PID/status files
+ * on the system. Be careful to handle the possibility that a process’s parent (and thus
+ * its /proc/PID directory) disappears during the scan of all /proc/PID directories.
  */
  
 #include "tlpi_hdr.h"
