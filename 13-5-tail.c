10613311a48d519aa81fc769e571bb36365b079e
munki man <thatfunkymunki@gmail.com>
2017-10-17 17:11:17 -0500

    solutions to 13-1 - 13-4 and initial commit for 13-5

---
+++ 13-5-tail.c
@@ -0,0 +1,11 @@
+/*Exercise 13-5
+ * The command tail [ -n num] file prints the last num lines (ten by default) of
+ * the named file. Implement this command using I/O system calls (lseek(), read(),
+ * write(), and so on). Keep in mind the buffering issues described in this chapter,
+ * in order to make the implementation efficient
+ */
+ 
+ #include "tlpi_hdr.h"
+ int main(int argc, char** argv){
+   return 0;
+ }
